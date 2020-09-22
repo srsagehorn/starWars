2c2cdc30e4d6b7f8e3911594948888cd0b9f90df
Shannon Sagehorn <srsagehorn@gmail.com>
2020-09-22 14:58:37 -0500

    front end html

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const characterData = require("./characterData");
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Sar Wars app");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api/characters", (req, res) => {
   res.json(characterData);
 });
 
-app.get("/api/characters/:character", (req, res) => {
+app.get("/api/:character", (req, res) => {
   var chosen = req.params.character;
   const found = characterData.find((char) => char.route == chosen);
 
   res.json(found || false);
   res.end;
+});
+
+app.get("/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.post("/add", (req, res) => {
+  const newCharacters = req.body;
+  characterData.push(newCharacters);
+  res.json(newCharacters);
+  res.end();
 });
 
 app.listen(PORT, () => {
